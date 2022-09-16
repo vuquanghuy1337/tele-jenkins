9e780b09ac6448eee1470620ba96c0998358c898
vuquanghuy1337 <vuquanghuy1337@gmail.com>
2022-09-16 07:26:39 +0000

    Change script

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
 def call() {
-	curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="Build status is "${currentBuild.result}" "
+            sh """ #!/bin/bash 
+            curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="Build status is "${currentBuild.result}" "
+            """
 }
