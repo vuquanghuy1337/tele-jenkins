0f00fd7c3d92426095204076a158842c21391493
vuquanghuy1337 <vuquanghuy1337@gmail.com>
2022-09-16 07:57:38 +0000

    Change variable

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
-def call() {
+def call(String botToken, String chatID) {
             sh """ #!/bin/bash 
-            curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="Build status is "${currentBuild.result}" "
+            curl -s -X POST https://api.telegram.org/bot${botToken}/sendMessage -d chat_id=${chatID} -d text="Build status is "${currentBuild.result}" "
             """
 }
