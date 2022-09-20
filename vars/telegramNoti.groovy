f8257c9a433204c78ab1e48135636eac46f245ed
vuquanghuy1337 <72914613+vuquanghuy1337@users.noreply.github.com>
2022-09-20 10:30:45 +0700

    Update telegramNoti.groovy

--- vars/telegramNoti.groovy
+++ vars/telegramNoti.groovy
@@ -4,7 +4,7 @@ def call(String API, String chatID, String bell){
 	sh """ #! /bin/bash
 	curl -X POST \
 	-H 'Content-Type: application/json' \
-	-d '{"text": "Job \\"${JOB_NAME}\\" has status ${currentBuild.result}, Build ID is ${BUILD_ID} \n\n View log in [${bell}]($BUILD_URL/console)", "parse_mode": "markdown" , "chat_id": '$chatID'}' \ 
+	-d '{"text": "Job \\"${JOB_NAME}\\" has status ${currentBuild.result}, Build ID is ${BUILD_ID} \n\n View log in [${bell}]($BUILD_URL/console)", "parse_mode": "markdown" , "chat_id": '$chatID'}' \\ 
 	https://api.telegram.org/$API/sendMessage 
 	"""
 }
